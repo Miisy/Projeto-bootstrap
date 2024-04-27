139dfa4beb67f3162dea3f07fa941e2060322e5e
Thamires Sales <thamisales@hotmail.com>
2024-04-27 17:35:05 -0300

    all changes in bootstrap

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#tel').mask('(00) 00000-0000')
+    
+    $('form').validate({
+        rules: {
+            nome:{
+                required:true
+            },
+            email: {
+                required:true,
+                email:true
+            },
+            mensagem:{
+                required:true
+            }
+        },
+        messages:{
+            name: 'Verifique novamente'
+        }
+    })
+})
